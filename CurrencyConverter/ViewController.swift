9663c44d2a93e3252208e3cdfbfda37a5e644168
dotDustin <logins.dmahone@gmail.com>
2020-07-09 00:59:59 -0400

    Initial Commit

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Dustin Mahone on 7/9/20.
+//  Copyright © 2020 Dustin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
